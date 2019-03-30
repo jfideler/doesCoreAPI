21f6189a1f92e601ef2d71bc1613061771b4f745
jfideler <jafideler@gmail.com>
2019-03-30 15:37:25 -0500

    Works on AWS!

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoesCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+
+            var jsonString = System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + "/assets/lazarus/works.json");
+            return jsonString.ToString();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Controllers/lazarusController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace DoesCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LazarusController : ControllerBase
+    {
+        // GET api/values
+        [EnableCors]  
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var jsonString = System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + "/assets/lazarus/works.json");
+            return jsonString.ToString();
+        }
+
+        // GET api/values/5
+        [EnableCors]  
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            var jsonString = System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + "/assets/lazarus/works.json");
+            var objects = JArray.Parse(jsonString); // parse as array 
+
+            var result = "value: " + id;
+            foreach (JObject root in objects)
+            {
+                var item = JObject.Parse(root.ToString());
+                var index = (string)item["index"];
+                if(index == id){
+                    result = root.ToString();
+                    break;
+                }
+            }
+            
+            return result;
+        }
+
+    }
+}
